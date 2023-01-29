f44bd01ccf2e526f2161d7d43b0ddb0f44c87378
hellside7 <slava382903@gmail.com>
2023-01-29 17:23:38 +0300

    первый коммит

---
+++ praktikOne/app.kt
@@ -0,0 +1,13 @@
+fun main()
+{
+    println("Введите т3-х значное число")
+    var a :Int = readLine().toString().toInt()
+    var b = a%10
+    var c = a/10 % 10
+    var h = a/100 % 10
+    println("$b and $c and $h")
+    var sum = b+c+h
+    println("сумма цифр числа = $sum")
+    var pr = b*c*h
+    println("их произведение = $pr")
+}
